9268f283f30aa732702bfa8ebd8d314ca5c9b91b
Chi-Feng Tsai <ktpss95112@yahoo.com.tw>
2018-12-25 10:29:22 +0800

    first commit with all things done

---
+++ README.md
@@ -0,0 +1,2 @@
+# metronome
+2018 Winter, costomized metronome delevoped using [pydub](https://github.com/jiaaro/pydub)
---
+++ main.py
@@ -0,0 +1,31 @@
+from pydub import AudioSegment
+
+tick = AudioSegment.from_mp3('tic.mp3')
+high_pitch = AudioSegment.from_mp3('tic(high_pitch).mp3')
+
+one_tick_duration = 1000 * 60 / 150
+half_tick_duration = one_tick_duration / 2
+
+error = 40
+
+first_tick = high_pitch[error:error+one_tick_duration]
+one_tick = tick[:one_tick_duration]
+half_tick = tick[:half_tick_duration]
+
+seven_over_eight = first_tick + one_tick * 2 + half_tick
+four_four = first_tick + one_tick * 3
+
+output = seven_over_eight * 18\
+       + four_four * 8\
+       + seven_over_eight * 8\
+       + seven_over_eight * 18\
+       + four_four * 8\
+       + seven_over_eight * (112 - 76)
+# starts from 49
+output2 = seven_over_eight * 2\
+       + seven_over_eight * 18\
+       + four_four * 8\
+       + seven_over_eight * (112 - 76)
+
+output.export('output(17).mp3', format='mp3')
+output2.export('output(49).mp3', format='mp3')
