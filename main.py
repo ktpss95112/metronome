7461a1ada23b118944db2cedf8d8355e2e141a85
Chi-Feng Tsai <ktpss95112@yahoo.com.tw>
2018-12-25 14:21:14 +0800

    add two measures to output(17).mp3, add some commentary in the code

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from pydub import AudioSegment
 
+"""
+There are two input files:
+    tic.mp3
+    tic(high_pitch).mp3
+
+There are two output files:
+    output(17).mp3
+    output(51).mp3
+# Note that the number in the parentheses indicates the measure to start 
+  singing, so there will be two empty measures in the beginning to have 
+  the singers prepared.
+"""
+
 tick = AudioSegment.from_mp3('tic.mp3')
 high_pitch = AudioSegment.from_mp3('tic(high_pitch).mp3')
 
@@ -15,7 +28,8 @@ half_tick = tick[:half_tick_duration]
 seven_over_eight = first_tick + one_tick * 2 + half_tick
 four_four = first_tick + one_tick * 3
 
-output = seven_over_eight * 18\
+output = seven_over_eight * 2\
+       + seven_over_eight * 18\
        + four_four * 8\
        + seven_over_eight * 8\
        + seven_over_eight * 18\
@@ -28,4 +42,4 @@ output2 = seven_over_eight * 2\
        + seven_over_eight * (112 - 76)
 
 output.export('output(17).mp3', format='mp3')
-output2.export('output(49).mp3', format='mp3')
+output2.export('output(51).mp3', format='mp3')
